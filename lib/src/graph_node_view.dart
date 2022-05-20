7cb0354e67fb827a279a179dd11fcd86cc40dd7e
xloc <xloc.cc@outlook.com>
2022-05-20 01:20:33 -0700

    split file

---
+++ lib/src/graph_node_view.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:knowledge_base_flutter/src/knowledge_base_page.dart';
+
+class GraphNodeView extends StatelessWidget {
+  final void Function(String nodeID, DragUpdateDetails)? onNodeDragUpdate;
+  final KnowledgeBaseView kbaseview;
+  final KnowledgeBase kbase;
+
+  const GraphNodeView({
+    Key? key,
+    required this.kbaseview,
+    required this.kbase,
+    this.onNodeDragUpdate,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: kbaseview.nodeLayout.entries.map((e) {
+        return Positioned(
+          left: e.value.dx,
+          top: e.value.dy,
+          child: Draggable(
+            onDragUpdate: ((details) {
+              if (onNodeDragUpdate != null) onNodeDragUpdate!(e.key, details);
+            }),
+            feedback: Container(),
+            child: Container(
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                border: Border.all(),
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.white,
+              ),
+              child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Text(kbase.getNode(e.key)!.title)),
+            ),
+          ),
+        );
+      }).toList(),
+    );
+  }
+}
